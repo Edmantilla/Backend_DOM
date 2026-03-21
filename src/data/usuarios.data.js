377ec43ab21468e60dc0dacc1b8c951d44f717af
Ximena <ximenaquiro02@gmail.com>
2026-03-20 19:22:50 -0500

    agregar archivos de datos de prueba para tareas y usuarios

---
+++ src/data/usuarios.data.js
@@ -0,0 +1,43 @@
+const users = [
+    {
+      "id": 1,
+      "name": "Michael Echeverry",
+      "username": "mecheverry",
+      "email": "michael@grupo01.com",
+      "ciudad": "Medellín",
+      "genero": "masculino"
+    },
+    {
+      "id": 2,
+      "name": "Edward Mantilla",
+      "username": "emantilla",
+      "email": "edward@grupo01.com",
+      "ciudad": "Bogotá",
+      "genero": "masculino"
+    },
+    {
+      "id": 3,
+      "name": "Ximena Quintanilla",
+      "username": "xquintanilla",
+      "email": "ximena@grupo01.com",
+      "ciudad": "Cali",
+      "genero": "femenino"
+    },
+    {
+      "id": 4,
+      "name": "Laura Gómez",
+      "username": "lgomez",
+      "email": "laura@grupo01.com",
+      "ciudad": "Barranquilla",
+      "genero": "femenino"
+    },
+    {
+      "id": 5,
+      "name": "Carlos Ruiz",
+      "username": "cruiz",
+      "email": "carlos@grupo01.com",
+      "ciudad": "Cartagena",
+      "genero": "masculino"
+    }
+];
+export default users;
