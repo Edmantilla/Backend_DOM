8ccffd25e1e0dc16e247d5bd97a17a9dece95fe2
Ximena <ximenaquiro02@gmail.com>
2026-03-20 19:22:55 -0500

    agregar nodemon, scripts dev y start, y simplificar app.js

--- src/app.js
+++ src/app.js
@@ -7,14 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    if (req.method === "OPTIONS") return res.sendStatus(200);
-    next();
-});
-
 app.get("/", (req, res) => {
     res.status(200).json({
         success: true,
