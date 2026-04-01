6033fd73b07b19b32ef77ba92b9438b0983d05c3
Ximena <ximenaquiro02@gmail.com>
2026-04-01 08:24:44 -0500

    Se agregan comentarios al código

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import app from "./src/app.js";
+import app from "./src/app.js";                                 // Importa la aplicación principal (toda la configuración de Express)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;                          // Define el puerto donde correrá el servidor
+                                                                // Usa el puerto del archivo .env o, si no existe, usa 3000 por defecto
 
-app.listen(PORT, () => {
-    console.log(`Servidor encendido en el puerto ${PORT}`);
+app.listen(PORT, () => {                                        // Inicia el servidor
+    console.log(`Servidor encendido en el puerto ${PORT}`);     // Muestra un mensaje en consola indicando que el servidor está activo
 });
